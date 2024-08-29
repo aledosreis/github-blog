db8d491f855f75099280eb8481ee68f8155fb1d2
Alessandro Reis <alessandrodreis23@gmail.com>
2024-08-29 12:41:40 -0300

    add font awesome icons

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,8 +1,16 @@
 import Markdown from "react-markdown";
-import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
-import {dracula} from 'react-syntax-highlighter/dist/esm/styles/prism'
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 import styles from "./post.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArrowUpRightFromSquare,
+  faCalendarDay,
+  faChevronLeft,
+  faComment,
+} from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const markdown = `**Programming languages all have built-in data structures, but these often differ from one language to another.** This article attempts to list the built-in data structures available in JavaScript and what properties they have. These can be used to build other data structures. Wherever possible, comparisons with other languages are drawn.\n
 
@@ -14,21 +22,35 @@ let foo = 42;   // foo is now a number
 foo = 'bar';    // foo is now a string
 foo = true;     // foo is now a boolean
 ~~~
-`
+`;
 
 export function Post() {
   return (
     <>
       <div className={styles.postInfo}>
         <div className={styles.postActions}>
-          <a href="/">VOLTAR</a>
-          <a href="https://github.com/aledosreis">VER NO GITHUB</a>
+          <a href="/">
+            <FontAwesomeIcon icon={faChevronLeft} />
+            VOLTAR
+          </a>
+          <a href="https://github.com/aledosreis">
+            VER NO GITHUB
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+          </a>
         </div>
         <h1>JavaScript data types and data structures</h1>
         <div className={styles.postMetrics}>
-          <span>aledosreis</span>
-          <span>Há 1 dia</span>
-          <span>5 comentários</span>
+          <span>
+            <FontAwesomeIcon icon={faGithub} />
+            aledosreis
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faCalendarDay} />
+            Há 1 dia
+          </span>
+          <span>
+            <FontAwesomeIcon icon={faComment} />5 comentários
+          </span>
         </div>
       </div>
 
@@ -37,24 +59,23 @@ export function Post() {
         children={markdown}
         components={{
           code(props) {
-            const {children, className, ...rest} = props
-            const match = /language-(\w+)/.exec(className || '')
+            const { children, className, ...rest } = props;
+            const match = /language-(\w+)/.exec(className || "");
             return match ? (
               <SyntaxHighlighter
-              PreTag="div"
-              children={String(children).replace(/\n$/, '')}
-              language={match[1]}
-              style={dracula}
+                PreTag="div"
+                children={String(children).replace(/\n$/, "")}
+                language={match[1]}
+                style={dracula}
               />
             ) : (
               <code {...rest} className={className}>
                 {children}
               </code>
-            )
-          }
+            );
+          },
         }}
       />
     </>
   );
 }
-
